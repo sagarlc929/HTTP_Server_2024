9394406e9e46c209fb542418b246fe5cc8354efd
Sajan Ghimire <122589374+54J4N@users.noreply.github.com>
2024-10-02 09:37:03 +0545

    feat: implement request URL handling with logging, serve homepage and about page, and return 404 for undefined routes

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-    console.log("New Req Rec.");
-    console.log(req.headers);
-    res.end("Hello From Server Aagain");
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    fs.appendFile('log.txt', log, (err, data) => {
+        switch (req.url) {
+            case '/home': res.end('HomePage');
+                break
+            case '/about': res.end("Its me nepali Bhai");
+                break
+            default:
+                res.end("404");
+        }
+        console.log("New Req Rec.");
+        console.log(req.headers);
+        res.end("Hello From Server Aagain");
+    });
 });
 
 myServer.listen(8000, () => console.log("Server Started!"));
