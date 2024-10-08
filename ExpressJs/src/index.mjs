8be8d085ddd94647828a41c0f8cb832dc7d32859
Sajan Ghimire <122589374+54J4N@users.noreply.github.com>
2024-10-08 19:06:20 +0545

    worked with getrequest ,used route param -express

--- ExpressJs/src/index.mjs
+++ ExpressJs/src/index.mjs
@@ -3,17 +3,36 @@ import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const mockUsers = [
+    { id: 1, username: "Jhon", displayName: "JHON" },
+    { id: 2, username: "jack", displayName: "Jack" },
+    { id: 3, username: "adam", displayName: "Adam" },
+];
 
-app.get("/", (request, response) => {
-    response.status(201).send({ msg: "Hello!" });
+app.get("/api/users", (request, response) => {
+    response.status(mockUsers);
 });
 
-app.get("api/users", (request, response) => {
-    response.send([
+app.get('/api/users/:id', (request, response) => {
+    console.log(request.params);
+
+    const parseId = parseInt(request.params.id);
+
+    console.log(parseId);
+
+    if (isNaN(parseId))
+        return response.status(400).send({msg: "Bad Request. Invalid."});
+    
+    const findUser = mockUsers.find((user) => user.id === parseId)
+
+    if (!findUser) return response.sendStatus(404);
+        return response.send(findUser);
+    /*response.send([
         { id: 1, username: "Jhon", displayName: "JHON" },
         { id: 2, username: "jack", displayName: "Jack" },
         { id: 3, username: "adam", displayName: "Adam" },
     ]);
+    */
 });
 
 app.get('/api/products', (request, response) => {
@@ -23,5 +42,3 @@ app.get('/api/products', (request, response) => {
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
 });
-
-//localhost:3000
